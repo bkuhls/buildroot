b1ead189e163401eafa01c49d5d7cd59b817c58d
Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
2016-07-06 12:24:08 +0200

    package/spidermonkey185: new package
    This is the old 1.8.5 branch of spidermonkey, used in Firefox 4.
    It is rather unmaintained, but some software still depend on it,
    such as couchdb.
    
    Signed-off-by: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
    
    Bernd: Updated the patch by Nicolas
    - rebased
    - switched python to python3
    - added myself to DEVELOPERS
    - updated patches to solve build problems found by test-pkg
      - patches 1-10 were taken from Fedora:
        https://src.fedoraproject.org/rpms/js/tree/f26
      - patches 11 & 13 are from Nicolas original patch series
      - patch 12 was added to fix armv5 support
      - patch 14 fixes build with gcc 14.x
    - HOST_CFLAGS was added to fix configure errors detected by test-pkg:
      - br-arcle-hs38:
        gcc: error: unrecognized command line option '-matomic';
        did you mean '-mbionic'?
      - br-xtensa-full:
        gcc: error: unrecognized command line option '-mlongcalls'
        gcc: error: unrecognized command line option '-mauto-litpools'
      - powerpc-ctng_e500v2-linux-gnuspe:
        gcc: error: unrecognized argument in option '-mabi=spe'
        gcc: note: valid arguments to '-mabi=' are: ms sysv
        gcc: error: unrecognized command line option '-mfloat-gprs=double';
        did you mean '-ffloat-store'?
    - JIT support was disabled for BR2_ARM_CPU_ARMV4 to fix:
      {standard input}:35: Error: selected processor does not support ARM mode `blx SetVMFrameRegs'
      {standard input}:37: Error: selected processor does not support ARM mode `blx PushActiveVMFrame'
      {standard input}:46: Error: selected processor does not support ARM mode `blx PopActiveVMFrame'
      {standard input}:55: Error: selected processor does not support ARM mode `blx js_InternalThrow'
      {standard input}:60: Error: selected processor does not support ARM mode `blx PopActiveVMFrame'
      {standard input}:69: Error: selected processor does not support ARM mode `blx ip'
      config/rules.mk:1475: recipe for target 'MethodJIT.o' failed
    - test-pkg also detected build errors on powerpc and x86_64:
      jsapi.cpp: In function ‘JSBool JS_ConvertArgumentsVA(JSContext*, uintN, jsval*, const char*, __va_list_tag*)’:
      jsapi.cpp:361:63: error: cannot convert ‘__va_list_tag**’ to ‘__va_list_tag (*)[1]’
       for argument ‘5’ to ‘JSBool TryArgumentFormatter(JSContext*, const char**, JSBool, jsval**, __va_list_tag (*)[1])’
                                           JS_ADDRESSOF_VA_LIST(ap))) {
      which were fixed by ac_cv_va_val_copy=no
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

---
+++ package/spidermonkey185/spidermonkey185.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# Spidermonkey (1.8.5)
+#
+################################################################################
+
+SPIDERMONKEY185_VERSION = 1.0.0
+SPIDERMONKEY185_SITE = http://ftp.mozilla.org/pub/js
+SPIDERMONKEY185_SOURCE = js185-$(SPIDERMONKEY185_VERSION).tar.gz
+SPIDERMONKEY185_SUBDIR = js/src
+SPIDERMONKEY185_LICENSE = MPL-1.1 or GPL-2.0+ or LGPL-2.1+
+SPIDERMONKEY185_INSTALL_STAGING = YES
+SPIDERMONKEY185_DEPENDENCIES = host-python3 host-perl
+
+# To detect endianess a host binary is built from jscpucfg.cpp which is
+# not cross-compile friendly, so we force endianess.
+# HOST_CFLAGS are forced to avoid problems when detecting host gcc
+# because configure would pass TARGET_CFLAGS to the host gcc.
+SPIDERMONKEY185_CONF_ENV = \
+	HOST_CFLAGS="$(HOST_CFLAGS)" \
+	HOST_CXXFLAGS="$(HOST_CXXFLAGS) -DFORCE_$(BR2_ENDIAN)_ENDIAN" \
+	$(if $(BR2_powerpc)$(BR2_x86_64),ac_cv_va_val_copy=no)
+
+# Mozilla mixes up target, host and build.  See the comment in configure.in
+# around line 360.  Also, nanojit fails to build on sparc64 with
+# #error "unknown nanojit architecture", so disable the JIT.
+# Disable JIT for armv4 because this CPU does not support the asm code
+# used in spidermonkey.
+SPIDERMONKEY185_CONF_OPTS = \
+	--target=$(GNU_TARGET_NAME) \
+	--build=$(GNU_TARGET_NAME) \
+	--host=$(GNU_HOST_NAME) \
+	$(if $(BR2_ARM_CPU_ARMV4),--disable-methodjit) \
+	$(if $(BR2_sparc64),--disable-tracejit)
+
+$(eval $(autotools-package))
