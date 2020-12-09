30404eb34a3a66bf03e06214053c31278b717902
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-12-09 22:04:46 +0100

    package/kodi-pvr-hts: fix build with gcc < 7.x

--- package/kodi-pvr-hts/kodi-pvr-hts.mk
+++ package/kodi-pvr-hts/kodi-pvr-hts.mk
@@ -8,6 +8,6 @@ KODI_PVR_HTS_VERSION = 8.3.3-Matrix
 KODI_PVR_HTS_SITE = $(call github,kodi-pvr,pvr.hts,$(KODI_PVR_HTS_VERSION))
 KODI_PVR_HTS_LICENSE = GPL-2.0+
 KODI_PVR_HTS_LICENSE_FILES = LICENSE.md
-KODI_PVR_HTS_DEPENDENCIES = kodi
+KODI_PVR_HTS_DEPENDENCIES = libplatform kodi
 
 $(eval $(cmake-package))
