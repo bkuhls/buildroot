a30f2596fa3ac18b81ae8edc003e44cdfb213e2e
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-10 22:00:37 +0100

    board/fli4l: add dropbear_localoptions.h

---
+++ board/fli4l/dropbear_localoptions.h
@@ -0,0 +1,5 @@
+#define DSS_PRIV_FILENAME "/etc/ssh/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/etc/ssh/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/etc/ssh/dropbear_ecdsa_host_key"
+#undef INETD_MODE
+#undef DO_MOTD
