0196105f41248865f834390e5a389b0283145ce5
Bernd Kuhls <bernd@kuhls.net>
2025-06-10 21:24:46 +0200

    package/kodi-vfs-*: mass version bump to Piers

--- package/kodi-vfs-rar/kodi-vfs-rar.mk
+++ package/kodi-vfs-rar/kodi-vfs-rar.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VFS_RAR_VERSION = 21.0.2-Omega
+KODI_VFS_RAR_VERSION = 0a4de90585bcdcbc2065d141a39430670e335a08
 KODI_VFS_RAR_SITE = $(call github,xbmc,vfs.rar,$(KODI_VFS_RAR_VERSION))
 KODI_VFS_RAR_LICENSE = unrar, GPL-2.0+
 KODI_VFS_RAR_LICENSE_FILES = lib/UnrarXLib/license.txt LICENSE.md
