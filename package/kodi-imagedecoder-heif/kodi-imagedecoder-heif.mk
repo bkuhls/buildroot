aa4f783f374ed77950592d1799c844fb44fd7acd
Bernd Kuhls <bernd@kuhls.net>
2025-06-10 21:23:01 +0200

    package/kodi-imagedecoder-*: mass version bump to Piers

--- package/kodi-imagedecoder-heif/kodi-imagedecoder-heif.mk
+++ package/kodi-imagedecoder-heif/kodi-imagedecoder-heif.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_IMAGEDECODER_HEIF_VERSION = 21.0.2-Omega
+KODI_IMAGEDECODER_HEIF_VERSION = edc3d318a60fd12f383232a347db8f91f44df99e
 KODI_IMAGEDECODER_HEIF_SITE = $(call github,xbmc,imagedecoder.heif,$(KODI_IMAGEDECODER_HEIF_VERSION))
 KODI_IMAGEDECODER_HEIF_LICENSE = GPL-2.0+
 KODI_IMAGEDECODER_HEIF_LICENSE_FILES = LICENSE.md
