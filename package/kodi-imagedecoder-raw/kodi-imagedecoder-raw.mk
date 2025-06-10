97f534bb63c88072aaf9b1ed2103e6cfd9935821
Bernd Kuhls <bernd@kuhls.net>
2025-06-10 21:23:01 +0200

    package/kodi-imagedecoder-*: mass version bump to Piers

--- package/kodi-imagedecoder-raw/kodi-imagedecoder-raw.mk
+++ package/kodi-imagedecoder-raw/kodi-imagedecoder-raw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_IMAGEDECODER_RAW_VERSION = 21.0.2-Omega
+KODI_IMAGEDECODER_RAW_VERSION = 731badf98751a4983fcfdc43c0cc37770aca9773
 KODI_IMAGEDECODER_RAW_SITE = $(call github,xbmc,imagedecoder.raw,$(KODI_IMAGEDECODER_RAW_VERSION))
 KODI_IMAGEDECODER_RAW_LICENSE = GPL-2.0+
 KODI_IMAGEDECODER_RAW_LICENSE_FILES = LICENSE.md
