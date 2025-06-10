182d1beac0a7e3bf92a46dc54a24d16bb62b2a10
Bernd Kuhls <bernd@kuhls.net>
2025-06-10 21:24:21 +0200

    package/kodi-pvr-*: mass version bump to Piers
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>

--- package/kodi-pvr-njoy/kodi-pvr-njoy.mk
+++ package/kodi-pvr-njoy/kodi-pvr-njoy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_NJOY_VERSION = 21.0.2-Omega
+KODI_PVR_NJOY_VERSION = 22.2.2-Piers
 KODI_PVR_NJOY_SITE = $(call github,kodi-pvr,pvr.njoy,$(KODI_PVR_NJOY_VERSION))
 KODI_PVR_NJOY_LICENSE = GPL-2.0+
 KODI_PVR_NJOY_LICENSE_FILES = LICENSE.md
