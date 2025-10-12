586e39eae2f576e839e91e4b835dfce5ef30f3f4
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 22:05:48 +0200

    package/python-lmdb: bump version to 1.7.3

--- package/python-lmdb/python-lmdb.mk
+++ package/python-lmdb/python-lmdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LMDB_VERSION = 1.6.2
+PYTHON_LMDB_VERSION = 1.7.3
 PYTHON_LMDB_SOURCE = lmdb-$(PYTHON_LMDB_VERSION).tar.gz
-PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/71/54/fb1d5918406a72fcf46fbfaece7c0349d88cd8685f5443a142ddd7dac05b
+PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/5e/87/b6f8bccab1ec1fe58866108c0da648715bc1488627edf9702dae6380613a
 PYTHON_LMDB_LICENSE = OLDAP-2.8
 PYTHON_LMDB_LICENSE_FILES = LICENSE
 PYTHON_LMDB_CPE_ID_VENDOR = py-lmdb_project
