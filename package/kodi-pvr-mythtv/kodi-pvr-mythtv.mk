1c2742307d17a5247e461ffc6bb1694f9aad024c
Bernd Kuhls <bernd@kuhls.net>
2025-12-08 23:39:54 +0100

    package/kodi-pvr-*: mass version bump to Piers

--- package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
+++ package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_MYTHTV_VERSION = 21.2.3-Omega
+KODI_PVR_MYTHTV_VERSION = d247afbda1cfb1d3c0cae335f113016df0758e1b
 KODI_PVR_MYTHTV_SITE = $(call github,janbar,pvr.mythtv,$(KODI_PVR_MYTHTV_VERSION))
 KODI_PVR_MYTHTV_LICENSE = GPL-2.0+
 KODI_PVR_MYTHTV_LICENSE_FILES = LICENSE.md
