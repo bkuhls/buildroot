d8c7a22f48e0020ad58bcaa1cda8e23b3c9a8a36
Bernd Kuhls <bernd@kuhls.net>
2026-02-01 14:05:27 +0100

    package/gettext-gnu: bump version to 1.0
    Release notes:
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=blob;f=NEWS;h=565fc8ec95e591a810be2cef179acd130ba8f9c4;hb=53bcae93fbd26e0e816f0e8e6dd3524ebdc67618
    
    Updated license hash due to upstream commit
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commitdiff;h=f58d3e697ecd8e5d73e9fbc558627870308856aa
    
    Disabled unneeded selinux support for host builds to avoid build errors
    on selinux-enabled hosts.
    
    Updated sed filter to include "install-tests" which was added with
    upstream commit:
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commitdiff;h=04f810d72742259c1c198d2505fe03fa330e9d7d
    
    Since upstream version 0.25 installed m4 files were moved from
    /usr/share/aclocal/ to /usr/share/gettext/m4/:
    https://lists.gnu.org/archive/html/bug-gettext/2025-05/msg00041.html
    https://bugzilla.redhat.com/show_bug.cgi?id=2366708#c10
    Added corresponding _AUTORECONF_OPTS to dovecot & e2fsprogs.
    
    Added more patches to fix e2fsprogs because of changes which break the
    usage of autopoint:
    https://github.com/tytso/e2fsprogs/issues/34#issuecomment-770090064
    
    Updated GETTEXT_TINY_EXTRA_GETTEXT_FILES due to various rename commits:
    
    - gettext-tools/misc/gettextize.in:
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commit;h=21a3caa8011d5bbe595f00451ba3b0264f3cea4b
    
    - gettext-tools/po/Makevars.template:
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commit;h=8c716a85ec676bbc3eaba21f973d83e846a62f0c
    
    - gettext-runtime/po/insert-header.sin &
      gettext-runtime/po/remove-potcdate.sin :
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commit;h=fe3b543701e51c9cfd66b96c0e9100b1a9f36e1a
    https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commit;h=9a4589466bb18cb018ea6588e6836f034ee37a44
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>

--- package/e2fsprogs/e2fsprogs.mk
+++ package/e2fsprogs/e2fsprogs.mk
@@ -20,8 +20,9 @@ HOST_E2FSPROGS_DEPENDENCIES = host-pkgconf host-util-linux
 
 # 0001-configure.ac-check-for-FS_IOC_READ_VERITY_METADATA-a.patch
 # the additional dependencies are only needed for autoreconf
+E2FSPROGS_AUTOPOINT = YES
 E2FSPROGS_AUTORECONF = YES
-E2FSPROGS_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
+E2FSPROGS_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive --include=$(HOST_DIR)/share/gettext/m4
 E2FSPROGS_DEPENDENCIES += host-gettext host-autoconf-archive
 HOST_E2FSPROGS_DEPENDENCIES += host-gettext host-autoconf-archive
 
