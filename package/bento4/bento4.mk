41d2eb4214b214f936343aff3941566cdf2e6391
Bernd Kuhls <bernd@kuhls.net>
2026-02-15 09:07:32 +0100

    package/bento4: bump version to 1.6.0-641-5-Piers
    https://github.com/xbmc/Bento4/releases
    
    Removed -std=c++11 from cflags due to upstream commit which added
    compatibility with c++20:
    https://github.com/xbmc/Bento4/commit/9380a868a74252fa53c80174a3e942168e6c0d85
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>

--- package/bento4/bento4.mk
+++ package/bento4/bento4.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BENTO4_VERSION = 1.6.0-641-Omega
+BENTO4_VERSION = 1.6.0-641-5-Piers
 BENTO4_SITE = $(call github,xbmc,Bento4,$(BENTO4_VERSION))
 BENTO4_INSTALL_STAGING = YES
 BENTO4_LICENSE = GPL-2.0+
@@ -20,6 +20,6 @@ endif
 
 BENTO4_CONF_OPTS += \
 	-DBUILD_APPS=OFF \
-	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11 -fPIC -DAP4_PLATFORM_BYTE_ORDER=$(BENTO4_BYTE_ORDER)"
+	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -fPIC -DAP4_PLATFORM_BYTE_ORDER=$(BENTO4_BYTE_ORDER)"
 
 $(eval $(cmake-package))
