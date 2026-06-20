f17a06dfbaf0e080d00d1a7ae369e8dfca7ed913
Bernd Kuhls <bernd@kuhls.net>
2026-06-20 10:41:08 +0200

    package/kodi-pvr-plutotv: bump version to 22.3.1-Piers
    Replaced dependency rapidjson with nlohmann-json:
    https://github.com/kodi-pvr/pvr.plutotv/commit/c6d0c6800421d2245922a497c3ff2994136ab374
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>

--- package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
+++ package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_PVR_PLUTOTV_VERSION = 21.3.3-Omega
+KODI_PVR_PLUTOTV_VERSION = 22.3.1-Piers
 KODI_PVR_PLUTOTV_SITE = $(call github,kodi-pvr,pvr.plutotv,$(KODI_PVR_PLUTOTV_VERSION))
 KODI_PVR_PLUTOTV_LICENSE = GPL-2.0+
 KODI_PVR_PLUTOTV_LICENSE_FILES = LICENSE.md
-KODI_PVR_PLUTOTV_DEPENDENCIES = kodi rapidjson
+KODI_PVR_PLUTOTV_DEPENDENCIES = json-for-modern-cpp kodi
 
 $(eval $(cmake-package))
